c0b383a533a8e279ec70202a6df12dae00330202
Timur Salekhov <saltimure@gmail.com>
2021-05-02 22:26:22 +0300

    postgresql hibernate test

--- src/main/java/com/example/diplom/database/User.java
+++ src/main/java/com/example/diplom/database/User.java
@@ -1,12 +1,44 @@
 package com.example.diplom.database;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.*;
 
 @Entity
+@Table(name="users")
 public class User {
+
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(nullable = false,unique = true,length = 45)
     private String email;
+
+    @Column(nullable = false,length = 64)
     private String password;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
---
+++ src/test/java/com/example/diplom/UserRepositoryTests.java
@@ -0,0 +1,34 @@
+package com.example.diplom;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.example.diplom.database.User;
+import com.example.diplom.database.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(false)
+public class UserRepositoryTests {
+    @Autowired
+    private UserRepository repo;
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Test
+    public void TestCreateUser() {
+        User user = new User();
+        user.setEmail("qweqweqwe@yandex.ru");
+        user.setPassword("12345");
+        User savedUser = repo.save(user);
+        User existUser = entityManager.find(User.class, savedUser.getId());
+        assertThat(existUser.getEmail()).isEqualTo(user.getEmail());
+    }
+}
